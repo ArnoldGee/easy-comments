9b91c8082c7e746bddb304791a193d69d6719e0a
Arnau Gómez Farell <agomez1f@gmail.com>
2020-12-21 19:35:55 +0100

    Added authentication (#2)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+
+import { ProvideAuth } from '../lib/auth'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ProvideAuth>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  );
+  
+    
 }
 
 export default MyApp
